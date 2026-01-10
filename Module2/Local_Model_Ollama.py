90bb77b4d5fdd9e45b78a85b7746eb0b5d99603b
Babak Forouraghi <bforoura@sju.edu>
2026-01-10 13:48:20 -0500

    Add files via upload

---
+++ Module2/Local_Model_Ollama.py
@@ -0,0 +1,17 @@
+from langchain_ollama import ChatOllama
+
+chat = ChatOllama(
+    model="deepseek-r1:1.5b",
+    temperature=0,
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant.",
+    ),
+    ("human", "What makes LangChain great for working with LLMs?"),
+]
+ai_msg = chat.invoke(messages)
+print(ai_msg.content)
+
